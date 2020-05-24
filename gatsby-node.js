49a15553c19e01d3db425b5d7ea29d3878556242
Kamran Ayub <kamran.ayub@gmail.com>
2020-05-24 15:08:15 -0500

    feat: Use Gatsby cache instead of generating a file
    - Add new `id` option (default: `default`) to change cache key

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,25 @@
 const typescript = require("typescript");
 const typedoc = require("typedoc");
-const path = require("path");
 const fs = require("fs");
 
 exports.sourceNodes = async (
-  { actions, createNodeId, createContentDigest },
+  { actions, createNodeId, createContentDigest, cache, reporter },
   configOptions
 ) => {
   const { createNode } = actions;
 
   delete configOptions.plugins;
 
+  const { src, id = "default", typedoc: typedocOptions = {} } = configOptions;
+
+  if (!src || !Array.isArray(src)) {
+    reporter.paninOnBuild(
+      "gatsby-source-typedoc requires a `src` array of TypeScript files to process"
+    );
+  }
+
   function processTypeDoc(generated) {
-    const nodeId = createNodeId(`typedoc-${generated.name || "default"}`);
+    const nodeId = createNodeId(`typedoc-${id}`);
     const nodeContent = JSON.stringify(generated);
 
     const nodeData = {
@@ -28,12 +35,17 @@ exports.sourceNodes = async (
     return nodeData;
   }
 
-  const { src, typedoc: typedocOptions = {} } = configOptions;
-
-  if (!src || !Array.isArray(src)) {
-    throw new Error(
-      "gatsby-source-typedoc requires a `src` array of TypeScript files to process"
+  //
+  // Use existing cached data, if already processed
+  //
+  const existing = await cache.get(`gatsby-source-typedoc--generated-${id}`);
+  if (existing) {
+    const nodeData = processTypeDoc(existing);
+    createNode(nodeData);
+    reporter.verbose(
+      `Using generated TypeDoc from previous build with ID: ${id}`
     );
+    return true;
   }
 
   const app = new typedoc.Application();
@@ -55,16 +67,32 @@ exports.sourceNodes = async (
 
   app.bootstrap(Object.assign({}, typedocOptions));
 
-  const generatedFile = path.join(__dirname, ".cache", "generated.json");
+  try {
+    const reflection = app.convert(src);
 
-  const generated = app.generateJson(src, generatedFile);
+    if (reflection) {
+      const eventData = {
+        outputDirectory: __dirname,
+        outputFile: `generated=${id}.json`,
+      };
+      const serialized = app.serializer.projectToObject(reflection, {
+        begin: eventData,
+        end: eventData,
+      });
+      const nodeData = processTypeDoc(serialized);
 
-  if (generated) {
-    const nodeData = processTypeDoc(JSON.parse(fs.readFileSync(generatedFile)));
-    createNode(nodeData);
-  } else {
-    console.error("Failed to generated TypeDoc JSON file");
+      // Store in Gatsby cache
+      await cache.set(`gatsby-source-typedoc--generated-${id}`, serialized);
+
+      createNode(nodeData);
+
+      reporter.verbose(`Generated TypeDoc and cached with ID: ${id}`);
+    } else {
+      reporter.warn("TypeDoc returned an empty project");
+    }
+  } catch (error) {
+    reporter.panicOnBuild("Encountered error when executing TypeDoc", error);
   }
 
-  return Promise.resolve(generated);
+  return Promise.resolve(true);
 };
